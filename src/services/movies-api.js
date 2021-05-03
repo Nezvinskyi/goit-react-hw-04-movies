5bd9fc946392684791aedc77a41691dd4af2ed8d
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-05-03 19:59:50 +0200

    adds api

---
+++ src/services/movies-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API = '4b778d4c29fb731b86ff7a9149d1de58';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const fetchMovies = async () => {
+  const response = await axios.get(
+    `${BASE_URL}/trending/all/day?api_key=${API}`,
+  );
+  return response.data.results;
+};
+
+const fetchById = async id => {
+  const response = await axios.get(`${BASE_URL}/movie/${id}?api_key=${API}`);
+  return response.data;
+};
+
+export default { fetchMovies, fetchById };
